e46c249535e817cfbcac917dba5aa0407136060a
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-10-21 18:51:54 -0300

    refactor: aprimora o layout do relatório PDF

--- src/main/java/carro/com/api/Service/CarroRelatorioService.java
+++ src/main/java/carro/com/api/Service/CarroRelatorioService.java
@@ -1,13 +1,14 @@
 package carro.com.api.Service;
 import java.io.OutputStream;
+import java.text.DecimalFormat;
 import java.util.List;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
-import com.itextpdf.text.Font.FontFamily;
-import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
@@ -18,9 +19,10 @@ import carro.com.api.Model.Carro;
 public class CarroRelatorioService {
 
     public void gerarRelatorioPdf(List<Carro> carros, OutputStream outputStream) {
-        Document document = new Document();
+    	
+        Document document = new Document(PageSize.A4, 50, 50, 50, 50);
         try {
-            PdfWriter.getInstance(document, outputStream);
+        	PdfWriter.getInstance(document, outputStream);
             document.open();
 
             Font font = new Font(Font.FontFamily.UNDEFINED, 18);
@@ -28,40 +30,54 @@ public class CarroRelatorioService {
             titulo.setAlignment(Element.ALIGN_CENTER);
             titulo.setSpacingAfter(20);
             document.add(titulo);
-
+            
             PdfPTable table = new PdfPTable(6);
-            table.addCell("Nome");
-            table.addCell("Preço");
-            table.addCell("Ano");
-            table.addCell("Marcas");
-            table.addCell("Cidade");
-            table.addCell("Placa");
+            table.setWidthPercentage(100);
+
+            // Definir o cabeçalho da tabela
+            String[] headers = {"Nome", "Preço", "Ano", "Marcas", "Cidade", "Placa"};
+            for (String header : headers) {
+                PdfPCell headerCell = new PdfPCell(new Paragraph(header));
+                headerCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                headerCell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                headerCell.setPadding(5);
+                table.addCell(headerCell);
+            }
+            
+        	String padrao = "###, ###. ##";
+        	DecimalFormat df = new DecimalFormat(padrao);
 
             for (Carro carro : carros) {
             	PdfPCell nomeCell = new PdfPCell(new Paragraph(carro.getNome()));
                 nomeCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                nomeCell.setPadding(5);
                 table.addCell(nomeCell);
 
-                PdfPCell precoCell = new PdfPCell(new Paragraph("R$ " + String.valueOf(carro.getPreco())));
+                String precoFormatado = df.format(carro.getPreco());
+                PdfPCell precoCell = new PdfPCell(new Paragraph("R$ " + precoFormatado));
                 precoCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                precoCell.setPadding(5);
                 table.addCell(precoCell);
 
                 PdfPCell anoCell = new PdfPCell(new Paragraph(String.valueOf(carro.getAno())));
                 anoCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                anoCell.setPadding(5);
                 table.addCell(anoCell);
 
                 PdfPCell marcasCell = new PdfPCell(new Paragraph(carro.getMarca()));
                 marcasCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                marcasCell.setPadding(5);
                 table.addCell(marcasCell);
 
                 PdfPCell cidadeCell = new PdfPCell(new Paragraph(carro.getCidade()));
                 cidadeCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cidadeCell.setPadding(5);
                 table.addCell(cidadeCell);
 
                 PdfPCell placaCell = new PdfPCell(new Paragraph(carro.getPlaca()));
                 placaCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                placaCell.setPadding(5);
                 table.addCell(placaCell);
-
             }
 
             document.add(table);
